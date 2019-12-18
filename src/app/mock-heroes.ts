761ec3bc35a4bb1d6f0da45d5b011daccc129a13
Bruno Brazuna (Retail) <BR40044528@wipro.com>
2019-12-18 13:27:16 -0300

    treinamento angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: "Tracer", name: 'Lena Oxton' },
+  { id: "Widowmaker", name: 'Amélie Lacroix' },
+  { id: "Parah", name: 'Fareeha Amari' },
+  { id: "Mercy", name: 'Angela Ziegler' },
+  { id: "Mei", name: 'Mei-Ling Zhou' },
+  { id: "D´va", name: 'Hana Song' },
+  { id: "Reaper", name: 'Gabriel Reyes' },
+  { id: "Soldier 76", name: 'John Francis "Jack" Morrison' },
+  { id: "Ana", name: 'Ana Amari' },
+  { id: "Lucio", name: 'Lúcio Correia dos Santos' }
+];
+
+
